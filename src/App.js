b26a6e7b47be7ae066ed3cd8c823155e8d237415
primalisa <primalisha112k@gmail.com>
2022-06-24 11:51:17 +0530

    fontchanges

--- src/App.js
+++ src/App.js
@@ -13,30 +13,30 @@ import kmeans from "./images/Figure_9.png";
 const App = () => {
   return (
     <div style={{ backgroundColor: "#F9F9F9" }}>
-      <h1 style={{ textAlign: "center", fontSize: "4rem", fontWeight: "bold" }}>
+      <h1 style={{ textAlign: "center", fontSize: "3rem", fontWeight: "bold" }}>
         Customer Segmentation Analysis Result
       </h1>
-      <h4 style={{ fontSize: "3.5rem" }}>
+      <h4 style={{ fontSize: "2.5rem" }}>
         Attributes considered for the analysis from the given dataset
       </h4>
       <ul>
-        <li style={{ fontSize: "2rem" }}>Gender</li>
-        <li style={{ fontSize: "2rem" }}>Age</li>
-        <li style={{ fontSize: "2rem" }}>Annual Income</li>
+        <li style={{ fontSize: "1.5rem" }}>Gender</li>
+        <li style={{ fontSize: "1.5rem" }}>Age</li>
+        <li style={{ fontSize: "1.5rem" }}>Annual Income</li>
         <ul>
-          <li style={{ fontSize: "2rem" }}>1 - Below 3 digit(0-999) </li>
-          <li style={{ fontSize: "2rem" }}>
-            2 -above 3 digit and below 4 digit(1000-9999)
+          <li style={{ fontSize: "1.5rem" }}>1 - Below 3 digit(0-999) </li>
+          <li style={{ fontSize: "1.5rem" }}>
+            1.5 -above 3 digit and below 4 digit(1000-9999)
           </li>
-          <li style={{ fontSize: "2rem" }}>3 -above 4 digit(>10000)</li>
+          <li style={{ fontSize: "1.5rem" }}>3 -above 4 digit(>10000)</li>
         </ul>
 
-        <li style={{ fontSize: "2rem" }}>Spending Score</li>
-        <li style={{ fontSize: "2rem" }}>Visiting Frequency</li>
+        <li style={{ fontSize: "1.5rem" }}>Spending Score</li>
+        <li style={{ fontSize: "1.5rem" }}>Visiting Frequency</li>
       </ul>
 
-      <h4 style={{ fontSize: "3.5rem" }}>Individual Analysis Result</h4>
-      <h4 style={{ fontSize: "2.5rem" }}>Gender Analysis</h4>
+      <h4 style={{ fontSize: "2.5rem" }}>Individual Analysis Result</h4>
+      <h4 style={{ fontSize: "2.0rem" }}>Gender Analysis</h4>
       <img
         style={{
           display: "block",
@@ -48,7 +48,7 @@ const App = () => {
         alt=""
       />
 
-      <h4 style={{ fontSize: "2.5rem" }}>Age Analysis</h4>
+      <h4 style={{ fontSize: "2.0rem" }}>Age Analysis</h4>
       <img
         style={{
           display: "block",
@@ -60,7 +60,7 @@ const App = () => {
         alt=""
       />
 
-      <h4 style={{ fontSize: "2.5rem" }}>Annual Income Analysis</h4>
+      <h4 style={{ fontSize: "2.0rem" }}>Annual Income Analysis</h4>
       <img
         style={{
           display: "block",
@@ -72,7 +72,7 @@ const App = () => {
         alt=""
       />
 
-      <h4 style={{ fontSize: "2.5rem" }}>Spending Score Analysis</h4>
+      <h4 style={{ fontSize: "2.0rem" }}>Spending Score Analysis</h4>
       <img
         style={{
           display: "block",
@@ -84,7 +84,7 @@ const App = () => {
         alt=""
       />
 
-      <h4 style={{ fontSize: "2.5rem" }}>Visiting Frequency Analysis</h4>
+      <h4 style={{ fontSize: "2.0rem" }}>Visiting Frequency Analysis</h4>
       <img
         style={{
           display: "block",
@@ -96,7 +96,7 @@ const App = () => {
         alt=""
       />
 
-      <h4 style={{ fontSize: "2.5rem" }}>Annual Income vs Gender Analysis</h4>
+      <h4 style={{ fontSize: "2.0rem" }}>Annual Income vs Gender Analysis</h4>
       <img
         style={{
           display: "block",
@@ -108,7 +108,7 @@ const App = () => {
         alt=""
       />
 
-      <h4 style={{ fontSize: "2.5rem" }}>Spending Score vs Analysis</h4>
+      <h4 style={{ fontSize: "2.0rem" }}>Spending Score vs Analysis</h4>
       <img
         style={{
           display: "block",
@@ -120,7 +120,7 @@ const App = () => {
         alt=""
       />
 
-      <h4 style={{ fontSize: "2.5rem" }}>Final Cluster Analysis By KMeans</h4>
+      <h4 style={{ fontSize: "2.0rem" }}>Final Cluster Analysis By KMeans</h4>
       <img
         style={{
           display: "block",
@@ -131,39 +131,39 @@ const App = () => {
         src={kmeans}
         alt=""
       />
-      <h4 style={{ fontSize: "2.5rem" }}>5 Clusters Data</h4>
+      <h4 style={{ fontSize: "2.0rem" }}>5 Clusters Data</h4>
       <ul>
         <li>
          
-          <a href="https://docs.google.com/spreadsheets/d/1u4jaWqejmCHve--KN3dqafkRJuRPBpuN/edit?usp=sharing&ouid=114263125916828760868&rtpof=true&sd=true" style={{ display: "block", fontSize: "2.5rem" }}>
+          <a href="https://docs.google.com/spreadsheets/d/1u4jaWqejmCHve--KN3dqafkRJuRPBpuN/edit?usp=sharing&ouid=114263125916828760868&rtpof=true&sd=true" style={{ display: "block", fontSize: "2.0rem" }}>
            
             Cluster 1
           </a>
         </li>
         <li>
           {" "}
-          <a href="https://docs.google.com/spreadsheets/d/1BpAr0jJ8Lr2zVl9o71ErKcPMtobXoyYB/edit?usp=sharing&ouid=114263125916828760868&rtpof=true&sd=true" style={{ display: "block", fontSize: "2.5rem" }}>
+          <a href="https://docs.google.com/spreadsheets/d/1BpAr0jJ8Lr2zVl9o71ErKcPMtobXoyYB/edit?usp=sharing&ouid=114263125916828760868&rtpof=true&sd=true" style={{ display: "block", fontSize: "2.0rem" }}>
             {" "}
             Cluster 2
           </a>
         </li>
         <li>
           {" "}
-          <a href="https://docs.google.com/spreadsheets/d/1_s9hKW_uzn20Cw0vp9RA5ogDYbUAkQ0i/edit?usp=sharing&ouid=114263125916828760868&rtpof=true&sd=true" style={{ display: "block", fontSize: "2.5rem" }}>
+          <a href="https://docs.google.com/spreadsheets/d/1_s9hKW_uzn20Cw0vp9RA5ogDYbUAkQ0i/edit?usp=sharing&ouid=114263125916828760868&rtpof=true&sd=true" style={{ display: "block", fontSize: "2.0rem" }}>
             {" "}
             Cluster 3
           </a>
         </li>
         <li>
           {" "}
-          <a href="https://docs.google.com/spreadsheets/d/13NV_ks-92I6vcG3n0LmNOCEg41f8gLrC/edit?usp=sharing&ouid=114263125916828760868&rtpof=true&sd=true" style={{ display: "block", fontSize: "2.5rem" }}>
+          <a href="https://docs.google.com/spreadsheets/d/13NV_ks-92I6vcG3n0LmNOCEg41f8gLrC/edit?usp=sharing&ouid=114263125916828760868&rtpof=true&sd=true" style={{ display: "block", fontSize: "2.0rem" }}>
             {" "}
             Cluster 4
           </a>
         </li>
         <li>
           {" "}
-          <a href="https://docs.google.com/spreadsheets/d/1euTnnmXk8igARzE_BdO4rBx5jX-gBe_0/edit?usp=sharing&ouid=114263125916828760868&rtpof=true&sd=true" style={{ display: "block", fontSize: "2.5rem" }}>
+          <a href="https://docs.google.com/spreadsheets/d/1euTnnmXk8igARzE_BdO4rBx5jX-gBe_0/edit?usp=sharing&ouid=114263125916828760868&rtpof=true&sd=true" style={{ display: "block", fontSize: "2.0rem" }}>
             {" "}
             Cluster 5
           </a>

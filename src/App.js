42c165b9a077636d083cff1871e91232e311bab4
primalisa <primalisha112k@gmail.com>
2022-06-24 12:43:49 +0530

    fontchanges

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ const App = () => {
         <ul>
           <li style={{ fontSize: "1.5rem" }}>1 - Below 3 digit(0-999) </li>
           <li style={{ fontSize: "1.5rem" }}>
-            1.5 -above 3 digit and below 4 digit(1000-9999)
+            2 -above 3 digit and below 4 digit(1000-9999)
           </li>
           <li style={{ fontSize: "1.5rem" }}>3 -above 4 digit(>10000)</li>
         </ul>
@@ -37,6 +37,7 @@ const App = () => {
 
       <h4 style={{ fontSize: "2.5rem" }}>Individual Analysis Result</h4>
       <h4 style={{ fontSize: "2.0rem" }}>Gender Analysis</h4>
+      <h2>Here you can also see the exact number of customers in each gender.</h2>
       <img
         style={{
           display: "block",
@@ -49,6 +50,7 @@ const App = () => {
       />
 
       <h4 style={{ fontSize: "2.0rem" }}>Age Analysis</h4>
+      <h2>Here a box plot is implemented and we have prioritized customers between the age range of 25%-75% of the minimum and maximum age mentioned.</h2>
       <img
         style={{
           display: "block",
@@ -73,6 +75,7 @@ const App = () => {
       />
 
       <h4 style={{ fontSize: "2.0rem" }}>Spending Score Analysis</h4>
+      
       <img
         style={{
           display: "block",
@@ -121,6 +124,7 @@ const App = () => {
       />
 
       <h4 style={{ fontSize: "2.0rem" }}>Final Cluster Analysis By KMeans</h4>
+      <h2>Two very impactful attributes are considered for the overall analysis</h2>
       <img
         style={{
           display: "block",
@@ -132,43 +136,49 @@ const App = () => {
         alt=""
       />
       <h4 style={{ fontSize: "2.0rem" }}>5 Clusters Data</h4>
+      <div style={{display:'flex',justifyContent:'flex-start'}}>
       <ul>
         <li>
          
           <a href="https://docs.google.com/spreadsheets/d/1u4jaWqejmCHve--KN3dqafkRJuRPBpuN/edit?usp=sharing&ouid=114263125916828760868&rtpof=true&sd=true" style={{ display: "block", fontSize: "2.0rem" }}>
            
-            Cluster 1
+            Click to see the customers in cluster 1
           </a>
         </li>
+        <br></br>
         <li>
           {" "}
           <a href="https://docs.google.com/spreadsheets/d/1BpAr0jJ8Lr2zVl9o71ErKcPMtobXoyYB/edit?usp=sharing&ouid=114263125916828760868&rtpof=true&sd=true" style={{ display: "block", fontSize: "2.0rem" }}>
             {" "}
-            Cluster 2
+            Click to see the customers in cluster 2
           </a>
         </li>
+        <br></br>
         <li>
           {" "}
           <a href="https://docs.google.com/spreadsheets/d/1_s9hKW_uzn20Cw0vp9RA5ogDYbUAkQ0i/edit?usp=sharing&ouid=114263125916828760868&rtpof=true&sd=true" style={{ display: "block", fontSize: "2.0rem" }}>
             {" "}
-            Cluster 3
+            Click to see the customers in cluster 3
           </a>
         </li>
+        <br></br>
         <li>
           {" "}
           <a href="https://docs.google.com/spreadsheets/d/13NV_ks-92I6vcG3n0LmNOCEg41f8gLrC/edit?usp=sharing&ouid=114263125916828760868&rtpof=true&sd=true" style={{ display: "block", fontSize: "2.0rem" }}>
             {" "}
-            Cluster 4
+            Click to see the customers in cluster 4
           </a>
         </li>
+        <br></br>
         <li>
           {" "}
           <a href="https://docs.google.com/spreadsheets/d/1euTnnmXk8igARzE_BdO4rBx5jX-gBe_0/edit?usp=sharing&ouid=114263125916828760868&rtpof=true&sd=true" style={{ display: "block", fontSize: "2.0rem" }}>
             {" "}
-            Cluster 5
+            Click to see the customers in cluster 5
           </a>
         </li>
       </ul>
+      </div>
     </div>
   );
 };
